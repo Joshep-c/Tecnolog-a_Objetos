9857e052531a6bb70d3e3d6fdc74c695df736b34
Joshep-c <121264024+Joshep-c@users.noreply.github.com>
2024-10-02 17:53:25 -0500

    última presentación de las partes implementadas, calculo de operadores básicos, sin uso de parentesís.

--- Laboratorio 2/ejercicio1.cpp
+++ Laboratorio 2/ejercicio1.cpp
@@ -2,22 +2,24 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <cctype>
 
 using namespace std;
 
-class CalculatorCore {
+// Clase que analiza la cadena de la operación
+class OperationAnalyzer {
 public:
-    int calculate(const string& expression) {
-        vector<int> numbers;
-        vector<char> operators;
+    void analyze(const string& expression, vector<int>& numbers, vector<char>& operators) {
         string temp = "";
 
         for (char ch : expression) {
             if (isdigit(ch)) {
                 temp += ch;
             } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-                numbers.push_back(stoi(temp));
-                temp = "";
+                if (!temp.empty()) {
+                    numbers.push_back(stoi(temp));
+                    temp = "";
+                }
                 operators.push_back(ch);
             }
         }
@@ -26,40 +28,43 @@ public:
             numbers.push_back(stoi(temp));
         }
 
-        int result = evaluatePriorityOperations(numbers, operators);
-        return result;
+        if (numbers.size() > 6) {
+            cerr << "Error: Se permiten como máximo 6 números." << endl;
+            numbers.clear();
+            operators.clear();
+        }
     }
+};
 
-private:
-    int evaluatePriorityOperations(vector<int>& numbers, vector<char>& operators) {
-        stack<int> numStack;
-        numStack.push(numbers[0]);
+// Clase que administra las operaciones matemáticas
+class CalculatorCore {
+public:
+    int calculate(const vector<int>& numbers, const vector<char>& operators) {
+        if (numbers.empty()) return 0;  // Si no hay números, retornar 0
 
-        for (size_t i = 0; i < operators.size(); ++i) {
-            char op = operators[i];
-            if (op == '*' || op == '/') {
-                int num1 = numStack.top();
-                numStack.pop();
-                int num2 = numbers[i + 1];
-                int result = (op == '*') ? num1 * num2 : num1 / num2;
-                numStack.push(result);
-            } else {
-                numStack.push(numbers[i + 1]);
+        vector<int> resultNumbers = numbers;
+        vector<char> resultOperators = operators;
+
+        for (size_t i = 0; i < resultOperators.size(); ++i) {
+            if (resultOperators[i] == '*' || resultOperators[i] == '/') {
+                int num1 = resultNumbers[i];
+                int num2 = resultNumbers[i + 1];
+                int result = (resultOperators[i] == '*') ? num1 * num2 : num1 / num2;
+
+                // Actualizar el vector con el resultado
+                resultNumbers[i] = result;
+                resultNumbers.erase(resultNumbers.begin() + i + 1);
+                resultOperators.erase(resultOperators.begin() + i);
+                i--;  // Volver a evaluar en la misma posición
             }
         }
 
-        return evaluateRemaining(numStack, operators);
-    }
-
-    int evaluateRemaining(stack<int>& numStack, vector<char>& operators) {
-        int result = numStack.top();
-        numStack.pop();
-
-        for (size_t i = 0; i < operators.size(); ++i) {
-            if (operators[i] == '+' || operators[i] == '-') {
-                int num = numStack.top();
-                numStack.pop();
-                result = (operators[i] == '+') ? result + num : result - num;
+        int result = resultNumbers[0];
+        for (size_t i = 0; i < resultOperators.size(); ++i) {
+            if (resultOperators[i] == '+') {
+                result += resultNumbers[i + 1];
+            } else if (resultOperators[i] == '-') {
+                result -= resultNumbers[i + 1];
             }
         }
 
@@ -67,15 +72,33 @@ private:
     }
 };
 
-int main() {
+class CalculatorProcessor {
+private:
+    OperationAnalyzer analyzer;
     CalculatorCore core;
+
+public:
+    void process(const string& expression) {
+        vector<int> numbers;
+        vector<char> operators;
+
+        analyzer.analyze(expression, numbers, operators);
+
+        if (!numbers.empty()) {
+            int result = core.calculate(numbers, operators);
+            cout << "Resultado: " << result << endl;
+        }
+    }
+};
+
+int main() {
+    CalculatorProcessor processor;
     string expression;
 
-    cout << "Ingrese una operacion matematica (max 6 numeros): ";
+    cout << "Ingrese una operación (máx 6 números): ";
     cin >> expression;
 
-    int result = core.calculate(expression);
-    cout << "Resultado: " << result << endl;
+    processor.process(expression);
 
     return 0;
 }
